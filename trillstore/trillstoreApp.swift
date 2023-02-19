258c2095b155369473c9e9a9e7be2819e688f3f9
WhitetailAni <87622301+RealKGB@users.noreply.github.com>
2023-02-19 00:58:31 -0600

    first commit!

---
+++ trillstore/trillstoreApp.swift
@@ -0,0 +1,18 @@
+//
+//  trillstoreApp.swift
+//  trillstore
+//
+//  Created by WhitetailAni on 2/17/23.
+//
+// Copyright (c) 2023 WhitetailAni
+
+import SwiftUI
+
+@main
+struct trillstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
